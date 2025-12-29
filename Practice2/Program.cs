eab19528049c5913865b07060a7c5ae139c9d178
mina6820 <minamedhat123456@gmail.com>
2025-12-29 15:46:23 +0200

    Add project files.

---
+++ Practice2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace Practice2
+{
+    internal class Program
+   {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! From The Other Side");
+           
+        }
+
+        static void Test(char[] input)
+        {
+            int count = 1;
+            int i = 0;
+            for(int j = 1; j < input.Length; j++)
+            {
+                if (input[i] == input[j])
+                {
+                    count++;
+                }
+                else
+                {
+                   input[i] = count.ToString()[0];
+                   count = 1;
+                    i++;
+                }
+  
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
